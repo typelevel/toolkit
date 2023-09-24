ed5c60819926d177ca04c5fd5c8ea99e7e5ef99f
Antonio Gelameris <toniogela89@gmail.com>
2023-09-24 14:35:20 +0200

    Adding the header

--- toolkit-testing/src/test/scala/org/typelevel/toolkit/ScalaCliProcess.scala
+++ toolkit-testing/src/test/scala/org/typelevel/toolkit/ScalaCliProcess.scala
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2023 Typelevel
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.typelevel.toolkit
 
 import fs2.io.process.{Process, ProcessBuilder, Processes}
@@ -38,7 +54,7 @@ object ScalaCliProcess {
 
   private def printStreams[F[_]: Concurrent: Console](
       process: Process[F]
-  ): F[Unit] = {
+  ): F[Unit] =
     Supervisor[F](await = true).use(supervisor =>
       for {
         _ <- process.stdout
@@ -55,8 +71,6 @@ object ScalaCliProcess {
           .supervise(supervisor)
       } yield ()
     )
-
-  }
 
   def command[F[_]: Processes: ApplicativeThrow: Concurrent: Console](
       args: List[String]
