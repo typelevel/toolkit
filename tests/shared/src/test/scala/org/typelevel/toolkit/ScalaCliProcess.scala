9c11a87c8d0d76a0ed3d492e2f8c43368c718246
Antonio Gelameris <toniogela89@gmail.com>
2023-09-26 09:25:57 +0200

    Temp fix for native

--- tests/shared/src/test/scala/org/typelevel/toolkit/ScalaCliProcess.scala
+++ tests/shared/src/test/scala/org/typelevel/toolkit/ScalaCliProcess.scala
@@ -84,9 +84,13 @@ object ScalaCliProcess {
   def command(args: List[String]): IO[Unit] = scalaCli(args)
 
   def run(body: String): IO[Unit] =
-    writeToFile(body)(false).use(f => scalaCli("run" :: f :: Nil))
+    writeToFile(body)(false).use(f =>
+      scalaCli("run" :: "--native-version" :: "0.4.15" :: f :: Nil)
+    )
 
   def test(body: String): IO[Unit] =
-    writeToFile(body)(true).use(f => scalaCli("test" :: f :: Nil))
+    writeToFile(body)(true).use(f =>
+      scalaCli("test" :: "--native-version" :: "0.4.15" :: f :: Nil)
+    )
 
 }
